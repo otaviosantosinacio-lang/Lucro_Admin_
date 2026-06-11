f6d089bbaea822047e96b91e51fb4f57bb1b76e1
Otavio Santos Inacio <otaviosantosinacio@gmail.com>
2026-06-11 15:24:26 -0300

    Organizando arquivos e criando base.py para modelos de tabelas do banco de dados

--- lucro_admin/infra/models/pedido.py
+++ lucro_admin/infra/models/pedido.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from sqlalchemy import ForeignKey, func
 from sqlalchemy.orm import Mapped, mapped_column, registry
 
+from lucro_admin.infra.models.situacao_pedidos_bling import Situacao_Pedido_Bling
 registro_tabela = registry()
 
 @registro_tabela.mapped_as_dataclass
@@ -12,4 +13,7 @@ class Pedido:
     id_pedido: Mapped[int] = mapped_column(init=False, primary_key=True)
     id_bling: Mapped[int] = mapped_column(unique=True)
     num_bling: Mapped[int] = mapped_column(unique=True)
-    id_situacao: Mapped[int]
+    id_situacao: Mapped[int] = mapped_column(
+        ForeignKey(Situacao_Pedido_Bling.id_situacao),
+        nullable=False   
+    )
---
+++ lucro_admin/infra/models/situacao_pedidos_bling.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from sqlalchemy import func, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, registry
+
+registro_tabela = registry()
+
+@registro_tabela.mapped_as_dataclass
+class Situacao_Pedido_Bling:
+    __tablename__ = 'situacoes_pedidos_bling'
+
+    id_situacao: Mapped[int] = mapped_column(init=False, primary_key=True)
+    id_situacao_bling: Mapped[int] = mapped_column(unique=True, nullable=False)
+    nome_situacao: Mapped[str] = mapped_column(nullable=False)
+    cor_situacao: Mapped[str] = mapped_column(nullable=False)
+    status: Mapped[str] = mapped_column(
+        Boolean,
+        init=False,
+        nullable=False,
+        default=True,
+        server_default='true',
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
