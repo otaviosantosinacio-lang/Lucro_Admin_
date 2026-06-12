8b248b054bda9fadc2b0de507fdc17938ed57aec
Otavio Santos Inacio <otaviosantosinacio@gmail.com>
2026-06-12 17:56:28 -0300

    Model DataBase NotaFiscal

--- lucro_admin/infra/models/nota_fiscal.py
+++ lucro_admin/infra/models/nota_fiscal.py
@@ -7,9 +7,10 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from lucro_admin.infra.models.base import registro_tabela
 
 if TYPE_CHECKING:
-
+    from lucro_admin.infra.models.usuario import Usuario
+    from lucro_admin.infra.models.pedido import Pedido
 @registro_tabela.mapped_as_dataclass
-class Nota_Fiscal:
+class NotaFiscal:
     
     __tablename__ = 'notas_fiscais'
 
@@ -18,26 +19,69 @@ class Nota_Fiscal:
         primary_key=True
     )
 
-    id_nf_bling: Mapped[int]= mapped_column(
+    id_nf_bling: Mapped[int | None]= mapped_column(
         nullable=True
     )
 
     id_pedido: Mapped[int]= mapped_column(
         ForeignKey('pedidos.id_pedido'),
-        nullable=False
+        nullable=False,
+        unique=True
     )
 
-    url_xml: Mapped[str]= mapped_column(
+    url_xml: Mapped[str | None]= mapped_column(
         nullable=True
     )
 
-    serie: Mapped[int]= mapped_column(
+    serie: Mapped[int | None]= mapped_column(
         nullable=True
     )
 
-    chave_acesso: Mapped[str]= mapped_column(
+    chave_acesso: Mapped[str | None]= mapped_column(
         unique=True,
         nullable=True
     )
 
-    data_emissao: Mapped[date]
+    data_emissao: Mapped[date | None]= mapped_column(
+        nullable=True
+    )
+
+    valor_nf: Mapped[Decimal | None]= mapped_column(
+        nullable=True
+    )
+
+    created_at: Mapped[datetime]= mapped_column(
+        init=False,
+        server_default=func.now()
+    )
+
+    created_user_id: Mapped[int]= mapped_column(
+        ForeignKey('usuarios.id_usuario'),
+        nullable=False
+    )
+
+    updated_at: Mapped[datetime]= mapped_column(
+        init=False,
+        server_default=func.now(),
+        onupdate=func.now()
+    )
+
+    updated_user_id: Mapped[int]= mapped_column(
+        ForeignKey('usuarios.id_usuario'),
+        nullable=False
+    )
+
+    nota_fiscal_pedido: Mapped['Pedido']= relationship(
+        foreign_keys= id_pedido,
+        init=False
+    )
+
+    created_user: Mapped['Usuario']= relationship(
+        foreign_keys= created_user_id,
+        init=False
+    )
+
+    updated_user: Mapped['Usuario']= relationship(
+        foreign_keys= updated_user_id,
+        init=False
+    )
