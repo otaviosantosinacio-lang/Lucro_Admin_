fac91606678a072033e658e4e573e8fab63868e1
Otavio Santos Inacio <otaviosantosinacio@gmail.com>
2026-06-15 07:05:52 -0300

    Update item_pedido.py

--- lucro_admin/infra/models/item_pedido.py
+++ lucro_admin/infra/models/item_pedido.py
@@ -4,6 +4,13 @@ from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey, func
 from lucro_admin.infra.models.base import registro_tabela
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from lucro_admin.infra.models.pedido import Pedidos
+    from lucro_admin.infra.modes.situacao_pedido_bling import (
+        SituacaoPedidoBling)
+    from lucro_admin.infra.models.produto import Produtos
 
 @registro_tabela.mapped_as_dataclass
 class ItemPedido:
@@ -56,5 +63,20 @@ class ItemPedido:
     )
 
     created_user_id: Mapped[int]= mapped_column(
-        
+        ForeignKey('usuarios.id_usuario'),
+        nullable=False
     )
+    
+    updated_at: Mapped[datetime]= mapped_column(
+        init=False,
+        server_default=func.now(),
+        onupgrade=func.now()
+    )
+    
+    updated_user_id: Mapped[int]= mapped_column(
+        ForeignKey('usuario.id_usuario'),
+        nullable=False
+    )
+    
+    
+    
