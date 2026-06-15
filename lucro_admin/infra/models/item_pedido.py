78e66ddea596c553ed8ad7ae77ac7abe5126d4f2
Otavio Santos Inacio <otaviosantosinacio@gmail.com>
2026-06-14 21:31:17 -0300

    Update model item_pedido

--- lucro_admin/infra/models/item_pedido.py
+++ lucro_admin/infra/models/item_pedido.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
-
+from decimal import Decimal
+from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey, func
 from lucro_admin.infra.models.base import registro_tabela
 
 @registro_tabela.mapped_as_dataclass
 class ItemPedido:
+
+    __tablename__= 'item_pedido'
 
     id_item_pedido: Mapped[int]= mapped_column(
         primary_key=True,
@@ -15,4 +18,43 @@ class ItemPedido:
     id_pedido: Mapped[int]= mapped_column(
         ForeignKey('pedidos.id_pedido'),
         nullable=False
+    )
+
+    id_situacao: Mapped[int]= mapped_column(
+        ForeignKey('situacoes_pedidos_bling.id_situacao'),
+        nullable=False
+    )
+
+    id_produto: Mapped[int]= mapped_column(
+        ForeignKey('produtos.id_produto'),
+        nullable=False
+    )
+
+    quantidade: Mapped[int]= mapped_column(
+        nullable=False
+    )
+
+    preco_custo: Mapped[Decimal]= mapped_column(
+        nullable=False
+    )
+
+    preco_venda_unitario: Mapped[Decimal]= mapped_column(
+        nullable=False
+    )
+
+    frete_item: Mapped[Decimal]= mapped_column(
+        nullable=False
+    )
+
+    comissao_item: Mapped[Decimal]= mapped_column(
+        nullable=False 
+    )
+
+    created_at: Mapped[datetime]= mapped_column(
+        init=False,
+        server_default=func.now()
+    )
+
+    created_user_id: Mapped[int]= mapped_column(
+        
     )
