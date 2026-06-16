69e40270501d4c3e946cec8085758e8c8a3bb522
Otavio Santos Inacio <otaviosantosinacio@gmail.com>
2026-06-16 07:38:26 -0300

    Created test for create product in database

--- tests/test_database.py
+++ tests/test_database.py
@@ -2,6 +2,7 @@ from sqlalchemy import select
 
 from lucro_admin.infra.models.usuario import Usuario
 from lucro_admin.infra.models.situacao_pedidos_bling import SituacaoPedidoBling
+from lucro_admin.infra.models.produto import Produto
 
 def teste_criar_usuario(session):
 
@@ -34,5 +35,37 @@ def teste_criar_situacao_pedido_bling(session):
     )
 
     assert resultado.nome_situacao == 'Atendido'
-
-
+    
+def teste_criar_produto(session):
+    
+    usuario= Usuario(
+        nome_usuario='otavio123',
+        email='otavio@lucro_admin.com',
+        senha_hash='otavio@123'
+    )
+    
+    produto= Produto(
+        id_produto_bling= 13579,
+        sku= 'LADM0001',
+        descricao_produto= 'gerenciador de lucro',
+        fornecedor= 'Lucro Admin',
+        preco_custo= 29.99
+        created_user_id: 1,
+        update_user_id: 1
+    )
+    
+    session.add(usuario)
+    session.flush()
+    
+    session.add(produto)
+    session.commit()
+    
+    resultado= session.scalar(
+        select(Produto).where(
+            Produto.id_produto_bling == 13579
+        )
+    )
+    
+    assert resultado.sku == 'LADM0001'
+    
+    
