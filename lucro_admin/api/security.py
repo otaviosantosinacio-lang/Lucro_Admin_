502245ac10d6da14326837a8492e65e696238d35
Otavio Santos Inacio <otaviosantosinacio@gmail.com>
2026-06-18 19:23:57 -0300

    JTW generate

--- lucro_admin/api/security.py
+++ lucro_admin/api/security.py
@@ -1,7 +1,7 @@
 from pwdlib import PasswordHash
 
-pwd_context = PasswordHash.recommended()
 
+pwd_context = PasswordHash.recommended()
 
 def get_password_hash(password: str):
     return pwd_context.hash(password)
@@ -9,3 +9,7 @@ def get_password_hash(password: str):
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
--- lucro_admin/utils/time.py
+++ lucro_admin/utils/time.py
@@ -1,11 +1,12 @@
 import logging
+from zoneinfo import ZoneInfo
 
 
 def somandosecs(segundos):
     logger = logging.getLogger('lucroadmin.utils.time')
     from datetime import datetime, timedelta
 
-    agora = datetime.now()
+    agora = datetime.now(tz=ZoneInfo('UTC'))
     expira = agora + timedelta(seconds=segundos)
     logger.info('Validade do access token calculada')
     return expira
