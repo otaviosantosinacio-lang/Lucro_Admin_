aac260360456435dfd621a0173aa3c5707f680bb
otaviosantosinacio-lang <otaviosantosinacio@gmail.com>
2026-06-29 17:39:22 -0300

    Aprimorated auth tests and add refresh token flow

--- lucro_admin/api/security.py
+++ lucro_admin/api/security.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-from jwt import DecodeError, decode, encode
+from jwt import ExpiredSignatureError, DecodeError, decode, encode 
 from pwdlib import PasswordHash
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -60,7 +60,8 @@ async def get_current_user(
             raise credentials_exception
     except DecodeError:
         raise credentials_exception
-
+    except ExpiredSignatureError:
+        raise credentials_exception
     user = await session.scalar(
         select(Usuario).where(Usuario.email == subject_email)
     )
